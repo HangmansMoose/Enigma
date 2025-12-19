56703c88989d4b462dfadcc337abcf5ef32f236d
HangmansMoose <trivial86@proton.me>
2025-12-19 16:33:47 +1100

    readded glad

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist .\Enigma\third_party\GLFW\premake5.lua cp Premake_files\GLFW_premake5.lua .\Enigma\third_party\GLFW\premake5.lua
-if not exist .\Enigma\third_party\Glad\premake5.lua cp Premake_files\glad_premake5.lua .\Enigma\third_party\Glad\premake5.lua
-if not exist .\Enigma\third_party\imgui\premake5.lua cp Premake_files\imgui_premake5.lua .\Enigma\third_party\imgui\premake5.lua
-call vendor\premake\premake5.exe vs2022
+if not exist .\Enigma\third_party\GLFW\premake5.lua copy Premake_files\GLFW_premake5.lua .\Enigma\third_party\GLFW\premake5.lua
+if not exist .\Enigma\third_party\Glad\premake5.lua copy Premake_files\glad_premake5.lua .\Enigma\third_party\Glad\premake5.lua
+if not exist .\Enigma\third_party\imgui\premake5.lua copy Premake_files\imgui_premake5.lua .\Enigma\third_party\imgui\premake5.lua
+call premake5.exe vs2022
 PAUSE
